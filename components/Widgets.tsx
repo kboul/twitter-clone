1c2edc497719fe8f9284738ac35783b6f3ab4dbd
Konstantinos Voulgaridis <kos.voulgaridis@gmail.com>
2022-05-07 21:17:10 +0300

    Feature/widgets (#2)
    * Build input with serch icon
    
    * Introduce twitter timeline embed

---
+++ components/Widgets.tsx
@@ -0,0 +1,23 @@
+import { SearchIcon } from '@heroicons/react/outline'
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
+
+export default function Widgets() {
+  return (
+    <div className="mt-2 px-2">
+      <div className="mt-2 flex items-center space-x-2 rounded-full bg-gray-100 p-3">
+        <SearchIcon className="h-5 w-5 text-gray-400" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          type="text"
+          placeholder="Search Twitter"
+        />
+      </div>
+
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="sonnysangha"
+        options={{ height: 1000 }}
+      />
+    </div>
+  )
+}
