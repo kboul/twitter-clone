25b123ff038dabbc22cddf6e183596ad1926145f
Konstantinos Voulgaridis <kos.voulgaridis@gmail.com>
2022-05-07 21:55:18 +0300

    Feature/responsive design (#3)
    * Divide screen into 12 grids
    
    * Remove widgets as screen decreases and make feed occupy bigger space at the same time
    
    * Change sidebar items style, hide text on small screens
    
    * Push content on left and right while on full screen mode, remove vertical scrollbar

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -9,7 +9,9 @@ export default function SidebarRow({ Icon, title }: SidebarRowProps) {
   return (
     <div className="group flex max-w-fit cursor-pointer items-center space-x-2 rounded-full px-4 py-3 hover:bg-gray-100">
       <Icon className="h-6 w-6" />
-      <p className="group-hover:text-twitter">{title}</p>
+      <p className="hidden text-base font-light group-hover:text-twitter md:inline-flex lg:text-xl">
+        {title}
+      </p>
     </div>
   )
 }
