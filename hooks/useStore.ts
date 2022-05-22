c29ad1a9239e5e7f5012e360acd4df0ab9d35130
Konstantinos Voulgaridis <kos.voulgaridis@gmail.com>
2022-05-22 21:20:04 +0300

    Fetch and show comments (#6)

--- hooks/useStore.ts
+++ hooks/useStore.ts
@@ -1,11 +1,13 @@
 import create from "zustand";
 
-import { Tweet } from "../typings";
+import { Comment, Tweet } from "../typings";
 
 const useStore = create<{
+  comments: Comment[];
   tweets: Tweet[];
   setGlobalState: (data: any) => void;
 }>(set => ({
+  comments: [] as Comment[],
   tweets: [] as Tweet[],
   setGlobalState: (data: any) => set({ ...data })
 }));
