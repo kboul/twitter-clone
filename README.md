6e8ef766f51e179904e49a7a2b926fbe7e3948c6
kboul <kos.voulgaridis@gmail.com>
2022-05-23 19:04:07 +0300

    Implement twitter login without the option of login with another acount using next/auth

--- README.md
+++ README.md
@@ -9,7 +9,13 @@ NEXT_PUBLIC_SANITY_DATASET=production
 NEXT_PUBLIC_SANITY_PROJECT_ID=your_sanity_project_id
 SANITY_API_TOKEN=your_sanity_api_token
 NEXT_PUBLIC_BASE_URL=your_localhost_url
+NEXTAUTH_URL=your_app_domain
+NEXTAUTH_SECRET=your_next_auth_secret
+TWITTER_CLIENT_ID=your_twitter_client_id
+TWITTER_CLIENT_SECRET=your_twitter_client_secret
 ```
+
+You need to sign in to the twitter developer portal and create a project to be able to derive the last two ids.
 
 ## Getting Started
 
--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -2,12 +2,15 @@ import { SVGProps } from "react";
 
 interface SidebarRowProps {
   Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  onClick?: () => void;
   title: string;
 }
 
-export default function SidebarRow({ Icon, title }: SidebarRowProps) {
+export default function SidebarRow({ Icon, onClick, title }: SidebarRowProps) {
   return (
-    <div className="group flex max-w-fit cursor-pointer items-center space-x-2 rounded-full px-4 py-3 hover:bg-gray-100">
+    <div
+      className="group flex max-w-fit cursor-pointer items-center space-x-2 rounded-full px-4 py-3 hover:bg-gray-100"
+      onClick={onClick}>
       <Icon className="h-6 w-6" />
       <p className="hidden text-base font-light group-hover:text-twitter md:inline-flex lg:text-xl">
         {title}
