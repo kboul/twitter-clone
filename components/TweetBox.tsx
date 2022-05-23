3eb555cd2def2a27d23e84db29c59bec2b595be4
kboul <kos.voulgaridis@gmail.com>
2022-05-23 23:12:38 +0300

    Implement addimg and showing image functionality

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent, useRef, useState } from "react";
 import {
   CalendarIcon,
   EmojiHappyIcon,
@@ -6,11 +7,27 @@ import {
   SearchCircleIcon
 } from "@heroicons/react/outline";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
 
 export default function TweetBox() {
-  const [inputValue, setInputValue] = useState<string>("");
   const { data: session } = useSession();
+  const [inputValue, setInputValue] = useState<string>("");
+  const [imageUrlBoxOpen, setImageUrlBoxOpen] = useState<boolean>(false);
+  const [uploadedImage, setUploadedIetimage] = useState<string>("");
+
+  const imageInputRef = useRef<HTMLInputElement>(null);
+
+  const handlePhotoIconClick = () =>
+    setImageUrlBoxOpen(prevState => !prevState);
+
+  const handleImageAddToTweet = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if (!imageInputRef.current?.value) return;
+    console.log(imageInputRef.current.value);
+    setUploadedIetimage(imageInputRef.current.value);
+    imageInputRef.current.value = "";
+    setImageUrlBoxOpen(false);
+  };
 
   return (
     <div className="flex space-x-2 p-5">
@@ -21,7 +38,7 @@ export default function TweetBox() {
       />
 
       <div className="flex flex-1 items-center pl-2">
-        <form className="flex flex-1 flex-col">
+        <div className="flex flex-1 flex-col">
           <input
             className="h-24 w-full text-xl outline-none placeholder:text-xl"
             onChange={e => setInputValue(e.target.value)}
@@ -32,7 +49,10 @@ export default function TweetBox() {
 
           <div className="flex items-center">
             <div className="flex flex-1 space-x-2 text-twitter">
-              <PhotographIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <PhotographIcon
+                className="transition-transdiv h-5 w-5 cursor-pointer duration-150 ease-out hover:scale-150"
+                onClick={handlePhotoIconClick}
+              />
               <SearchCircleIcon className="h-5 w-5" />
               <EmojiHappyIcon className="h-5 w-5" />
               <CalendarIcon className="h-5 w-5" />
@@ -45,7 +65,31 @@ export default function TweetBox() {
               Tweet
             </button>
           </div>
-        </form>
+
+          {imageUrlBoxOpen && (
+            <form className="mt-5 flex rounded-lg bg-twitter/80 py-2 px-4 ">
+              <input
+                className="flex-1 bg-transparent p-2 text-white outline-none placeholder:text-white"
+                placeholder="Enter image url..."
+                ref={imageInputRef}
+                type="text"
+              />
+              <button
+                className="font-bold text-white"
+                onClick={handleImageAddToTweet}>
+                Add image
+              </button>
+            </form>
+          )}
+
+          {uploadedImage && (
+            <img
+              alt="uploaded image"
+              className="mt-10 h-40 w-full rounded-xl object-contain shadow-lg"
+              src={uploadedImage}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
