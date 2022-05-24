0ca1d4e2f091f2062a728fc9859b10a3b4aadddf
kboul <kos.voulgaridis@gmail.com>
2022-05-24 10:28:38 +0300

    Build functionality for adding a tweet

--- hooks/useStore.ts
+++ hooks/useStore.ts
@@ -4,12 +4,18 @@ import { Comment, Tweet } from "../typings";
 
 const useStore = create<{
   comments: Comment[];
+  imageUrlBoxOpen: boolean;
+  tweetInput: string;
   tweets: Tweet[];
   setGlobalState: (data: any) => void;
+  uploadedImage: string;
 }>(set => ({
   comments: [] as Comment[],
+  imageUrlBoxOpen: false,
+  tweetInput: "",
   tweets: [] as Tweet[],
-  setGlobalState: (data: any) => set({ ...data })
+  setGlobalState: (data: any) => set({ ...data }),
+  uploadedImage: ""
 }));
 
 export default useStore;
